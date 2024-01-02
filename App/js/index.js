8a39096d123b817988ac99ca2806aaed9161e13d
mushariq1 <155421788+mushariq1@users.noreply.github.com>
2024-01-02 16:27:48 +0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,17 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label7")
+                .setLeft("0.7619047619047619em")
+                .setTop("3.0476190476190474em")
+                .setWidth("12.342857142857143em")
+                .setHeight("3.2em")
+                .setCaption("Enter Artisan Name")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
